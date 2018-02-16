6c33cc4a61ffaf162555c76760c50707224b9f3c
alandgelber@gmail.com <alandgelber@gmail.com>
2018-02-16 23:20:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,15 +5,15 @@ function theBeatlesPlay(musicians, instruments) {
   }
   return allMusicians;
 }
+function johnLennonFacts(facts) {
+  const shoutedFacts = []
 
 const facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
-  
-function johnLennonFacts() {
-  var newFacts=[];
-  var i = 0;
-  while( i < facts.length) {
-    newFacts.push(facts[i] + "!!!");
-    i++;
+  let i = 0
+  while (i < facts.length) {
+    shoutedFacts.push(`${facts[i]}!!!`)
+    i++
   }
-  return newFacts;
+
+  return shoutedFacts
 }
